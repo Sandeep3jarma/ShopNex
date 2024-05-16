f2026f6bf0ee6a2b4ca41a46cb33298cd62d7afc
Sandeep3jarma <sandeepjarma3@gmail.com>
2024-05-16 15:27:48 +0530

    Contact us page change

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -9,10 +9,10 @@ const Contact = () => {
       <h1 id="myheading">
         Contact Us
       </h1>
-      <p>
+      <p className="jarma">
       This is the official page of Shopnix, where you can share all your queries, feedback, complaints, or any concern you may have about our products.
       </p>
-      <p>
+      <p className="sandeep">
       In Case of any grievance, don't hesitate to get in touch with us on our official contact number xxxxxxxxxx. Or you can write to us at xyz@gmail.com.
       </p>
 
